3aefff3811c141749d6ccb4542ee0bf98289cc1d
vmcod3r <valoirmoura@outlook.com>
2023-11-03 09:01:27 -0300

    section 13-graphql

---
+++ 13-graphql/cmd/server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/valoirmoura/13-graphql/internal/database"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/valoirmoura/13-graphql/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+	categoryDB := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+		CourseDB:   courseDB,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
